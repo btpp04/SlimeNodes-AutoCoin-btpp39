0a8160ea3cc7fae92c5fce35e54abf51c8fd8195
btpp03 <btpp03@gmail.com>
2026-05-23 12:39:05 +0000

    debug: show proxy status in OAuth step

--- makecoins.py
+++ makecoins.py
@@ -47,6 +47,7 @@ def send_tg(msg):
 
 def discord_oauth(tok):
     log("Discord OAuth...")
+    log(f"Proxy: PX={PX!r} px()={px()!r}")
     url = (f"https://discord.com/api/v9/oauth2/authorize"
            f"?client_id={CID}&scope={quote(SCO)}"
            f"&response_type=code&redirect_uri={quote(BASE+'/callback',safe='')}")
